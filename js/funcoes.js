eab6634b062b8f995ada14ffca2285169852b6c4
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-04-06 19:41:30 -0300

    adição de array no js , criação de variavel e adição de imagens

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,4 +1,8 @@
-
-let series = ["prison break","breaking bad" ,"stranger things"];
+//array = caixa onde se guarda as informações separadas
+let series = ["prison break" , "breaking bad" , "stranger things"];
 console.log(series);
 console.log(series.length);
+
+for(let i = 0; 1 < series.length; i++){
+    console.log(series[i]);
+}
